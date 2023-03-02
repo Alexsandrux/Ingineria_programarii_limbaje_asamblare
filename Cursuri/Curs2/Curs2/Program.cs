816f6598624f69bbd144736d4edeb4937910af91
Alexsandrux <paducelalexandru@gmail.com>
2023-03-02 19:21:36 +0200

    Curs 2 final

--- Cursuri/Curs2/Curs2/Program.cs
+++ Cursuri/Curs2/Curs2/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace Curs2
+{
+
+    //interface IObservator
+    //{
+    //    void Notificare();
+
+    //}
+
+    //interface IObservabil
+    //{
+    //    void Cupleaza(IObservator obj);
+
+    //    void Decupleaza(IObservator obj);
+    //}
+
+    //class Observabil : IObservabil
+    //{
+    //    protected List<IObservator> observatori = new List<IObservator>();
+    //    public void Cupleaza(IObservator obj)
+    //    {
+    //        observatori.Add(obj);
+    //    }
+
+    //    public void Decupleaza(IObservator obj)
+    //    {
+    //        observatori.Remove(obj);
+    //    }
+
+
+    //}
+
+    //class Consola : IObservator
+    //{
+    //    public void Notificare()
+    //    {
+    //        Console.WriteLine("Modificare stare!!");
+    //    }
+    //}
+
+    //class Element : Observabil
+    //{
+    //    int x;
+
+    //    public Element()
+    //    {
+    //        x = 0;
+    //    }
+
+    //    public int Valore { get { return x; } set { if(value != x) x = value; foreach (IObservator item in observatori)
+    //            {
+    //                item.Notificare();   
+    //            }
+    //        } }
+    //}
+
+    class Consola 
+    {
+        public void Notificare()
+        {
+            Console.WriteLine("Modificare stare!!");
+        }
+    }
+
+    class Element
+    {
+        int x;
+
+        public delegate void del_functie(); // delegate functie
+
+        public event del_functie evenimentSchimba;
+
+        public Element()
+        {
+            x = 0;
+        }
+
+        public int Valore
+        {
+            get { return x; }
+            set
+            {
+                if (value != x)
+                {
+                    x = value;
+                    if (evenimentSchimba != null)
+                        evenimentSchimba();
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Consola obiectConsola = new Consola();
+
+            Element obiectElement = new Element();
+
+            //obiectElement.Cupleaza(obiectConsola);
+
+            //obiectElement.Valore = 100;
+            //Console.ReadLine();
+            //obiectElement.Decupleaza(obiectConsola);
+
+            //obiectElement.Valore = 200;
+
+            obiectElement.evenimentSchimba += obiectConsola.Notificare;
+            obiectElement.Valore = 100;
+            Console.ReadLine();
+            obiectElement.evenimentSchimba -= obiectConsola.Notificare;
+            obiectElement.Valore = 100;
+        }
+    }
+}
